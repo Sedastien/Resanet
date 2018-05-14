db256797b439731af2c189f12059b8d8a2cbdb57
Sedastien <root@slam.sio.lla.fr>
2018-05-14 09:41:14 +0200

    resanet

--- github.sh
+++ github.sh
@@ -4,11 +4,12 @@
 git remote add origin https://github.com/Sedastien/resanet
 git init
 
-while [ nomFichier != 'exit' ]
+while [ nomFichier != 'test.txt' ]
 do
 	nomFichier="/home/developpeur/Bureau/projets/*"
 	git add $nomFichier
 	git status
 	git commit -a -m 'resanet'
-	git push -u origin master
 done
+
+git push -u origin master
